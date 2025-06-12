d73d7dc985470e7ec5041746a4e07c5980329fb7
Diagomezpa <Diagomezpa@gmail.com>
2025-06-12 18:47:45 -0500

    seccion 5 video 47 prefer const

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MyApp() );
+  runApp( const MyApp() );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Center(child: Text('Hola Mundo')),
+    return const MaterialApp(
+      home: Center(child:  Text('Hola Mundo')),
     );
   }
 }
