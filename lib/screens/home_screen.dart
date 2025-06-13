ff3488471a9b40d78bb927fb2720093d7f4d2f50
Diagomezpa <Diagomezpa@gmail.com>
2025-06-12 19:50:14 -0500

    seccion 5 video 51 mainAxisAlignment y columnas

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,6 +7,33 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child:  Text('Hola Mundo home_screen.dart'));
+    // El método build es donde se construye la interfaz de usuario del widget.
+    // el build context es el contexto en el que se construye el widget.
+    // El contexto es un objeto que contiene información sobre el árbol de widgets
+
+
+    // scaffold es un widget que proporciona una estructura básica para la aplicación. es como un lienzo en blanco
+    // donde podemos agregar otros widgets como AppBar, Drawer, BottomNavigationBar, etc.
+    // Scaffold es un widget que proporciona una estructura básica para la aplicación.
+
+    return  Scaffold( // le quitamos el const porque el appbar va a cambiar despues de construida la aplicacion porque el por ejemplo puede cambiar el tirulo  
+      // 
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        elevation: 10.0, // eleva el appbar 10 puntos
+      ),
+      backgroundColor: Colors.white,
+      body:   Center(
+      child: Column(
+      mainAxisAlignment: MainAxisAlignment.center, // centra los widgets en el eje vertical
+      
+        children: const [
+          Text('clicks counter'),
+          const SizedBox(height: 20), // espacio entre widgets
+          Text('40')
+        ],
+      ),
+      ),
+    );
   }
 }
